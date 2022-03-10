73969c52200b7676833c3f04d026ccd01f560134
AntenorLopez <LE18080561@merida.tecnm.mx>
2022-03-10 16:23:37 -0600

    ng2-angular-table

---
+++ src/app/angular-table/angular-table.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-angular-table',
+  templateUrl: './angular-table.component.html',
+  styleUrls: ['./angular-table.component.css']
+})
+export class AngularTableComponent implements OnInit {
+
+  settings = {
+    columns: {
+      id: {
+        title: 'ID',
+        filter: false
+      },
+      name: {
+        title: 'Full Name',
+        filter: false
+      },
+      username: {
+        title: 'User Name',
+        filter: false
+      },
+      email: {
+        title: 'Email',
+        filter: false
+      }
+    }
+  };
+
+  data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv"
+    },
+    
+    // ... list of items
+    
+    {
+      id: 11,
+      name: "Nicholas DuBuque",
+      username: "Nicholas.Stanton",
+      email: "Rey.Padberg@rosamond.biz"
+    }
+  ];
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { AngularTableComponent } from './angular-table/angular-table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularTableComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
